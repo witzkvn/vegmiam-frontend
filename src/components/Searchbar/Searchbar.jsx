315d5a92cab34904f622831b07c4decc0ad70400
Wikode Medias <wikodemedias@gmail.com>
2021-04-12 20:43:01 +0200

    fixed searchbar name to ref for app.js

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+import "./Searchbar.scss";
+
+const Searchbar = () => {
+  const [search, setSearch] = useState();
+
+  const handleChange = (e) => {
+    const newSearch = e.target.value;
+    setSearch(newSearch);
+  };
+
+  // test
+
+  return (
+    <div className="Searchbar">
+      <input type="text" placeholder="Recherche..." onChange={handleChange} value={search} />
+      <div className="Searchbar__button CustomButton CustomButton-primary">Manger !</div>
+    </div>
+  );
+};
+
+export default Searchbar;
