46a723480340bc1bcd1fcebea669f5e2b8302d4e
Wikode medias <wikode.medias@gmail.com>
2022-06-23 16:08:09 +0200

    client correction

--- src/index.js
+++ src/index.js
@@ -1,51 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import axios from "axios";
-import { Provider } from 'react-redux'
-import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom'
-import history from "./helper/functions/createBrowserHistory"
-import { authHeader } from "./helper/functions/auth-header"
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import history from "./helper/functions/createBrowserHistory";
+import { authHeader } from "./helper/functions/auth-header";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 // axios settings
 const getHeadersKeys = () => {
   return {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': authHeader()
-  }
-}
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: authHeader(),
+  };
+};
 
-export const client = () => {
-  return axios.create({
-    baseURL: 'http://localhost:3000/api/v1/',
-    headers: getHeadersKeys()
-  })
-}
-
-// export const client = async () => {
-//   if (process.env.NODE_ENV === "development") {
-//     return axios.create({
-//       baseURL: 'http://localhost:3000/api/v1/',
-//       headers: await headersKeys,
-
-//     })
-//   } else if (process.env.NODE_ENV === "devonline") {
-//     return axios.create({
-//       baseURL: 'https://vegmiam-back-dev.herokuapp.com/',
-//       headers: await headersKeys,
-//     })
-//   } else if (process.env.NODE_ENV === "production") {
-//     return axios.create({
-//       baseURL: 'https://vegmiam-back-master.herokuapp.com/',
-//       headers: await headersKeys,
-//     });
-//   }
+// export const client = () => {
+//   return axios.create({
+//     baseURL: 'http://localhost:3000/api/v1/',
+//     headers: getHeadersKeys()
+//   })
 // }
 
+export const client = async () => {
+  if (process.env.NODE_ENV === "development") {
+    return axios.create({
+      baseURL: "http://localhost:3000/api/v1/",
+      headers: getHeadersKeys(),
+    });
+  } else if (process.env.NODE_ENV === "devonline") {
+    return axios.create({
+      baseURL: "https://vegmiam-back-dev.herokuapp.com/",
+      headers: getHeadersKeys(),
+    });
+  } else if (process.env.NODE_ENV === "production") {
+    return axios.create({
+      baseURL: "https://vegmiam-back-master.herokuapp.com/",
+      headers: getHeadersKeys(),
+    });
+  }
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -55,7 +53,7 @@ ReactDOM.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
