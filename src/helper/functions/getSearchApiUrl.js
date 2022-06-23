fb5de47bf3f64485c4743571cc312ab521d4a737
Wikode medias <wikode.medias@gmail.com>
2022-06-23 15:11:32 +0200

    small adjustments

--- src/helper/functions/getSearchApiUrl.js
+++ src/helper/functions/getSearchApiUrl.js
@@ -1,6 +1,6 @@
 export const getSearchApiUrl = (searchParamsObj) => {
   if (searchParamsObj) {
-    return `${searchParamsObj?.searchWords ? ('&query=' + encodeURI(searchParamsObj.searchWords)) : ''}${searchParamsObj?.searchDifficulty?.length > 0 ? ('&difficulty=' + searchParamsObj?.searchDifficulty.join(',')) : ''}${searchParamsObj?.searchDuration?.length > 0 ? ('&time[lte]=' + Math.max(...searchParamsObj?.searchDuration)) : ''}${searchParamsObj?.searchOrder ? ('&sort=' + searchParamsObj?.searchOrder) : ''}${searchParamsObj?.searchCategory?.length > 0 ? ('&category=' + searchParamsObj?.searchCategory.join(',')) : ''}${searchParamsObj?.searchPage ? ('&page=' + searchParamsObj.searchPage) : ''}`.trim()
+    return `${searchParamsObj?.searchWords ? ('&text[search]=' + encodeURI(searchParamsObj.searchWords)) : ''}${searchParamsObj?.searchDifficulty?.length > 0 ? ('&difficulty=' + searchParamsObj?.searchDifficulty.join(',')) : ''}${searchParamsObj?.searchDuration?.length > 0 ? ('&time[lte]=' + Math.max(...searchParamsObj?.searchDuration)) : ''}${searchParamsObj?.searchOrder ? ('&sort=' + searchParamsObj?.searchOrder) : ''}${searchParamsObj?.searchCategory?.length > 0 ? ('&category=' + searchParamsObj?.searchCategory.join(',')) : ''}${searchParamsObj?.searchPage ? ('&page=' + searchParamsObj.searchPage) : ''}`.trim()
   }
   return ""
 }
