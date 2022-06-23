6db81a6f7ead54e03a3fe40fa7d8211e5f366568
Wikode medias <wikode.medias@gmail.com>
2022-06-23 23:47:11 +0200

    base url axios prod

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import reportWebVitals from "./reportWebVitals";
 
 // axios settings
 const getHeadersKeys = () => {
+  console.log("headerskeys");
   return {
     Accept: "application/json",
     "Content-Type": "application/json",
@@ -19,31 +20,37 @@ const getHeadersKeys = () => {
   };
 };
 
-// export const client = () => {
-//   return axios.create({
-//     baseURL: 'http://localhost:3000/api/v1/',
-//     headers: getHeadersKeys()
-//   })
-// }
-
 export const client = () => {
-  if (process.env.NODE_ENV === "development") {
-    return axios.create({
-      baseURL: "http://localhost:3000/api/v1/",
-      headers: getHeadersKeys(),
-    });
-  } else if (process.env.NODE_ENV === "devonline") {
-    return axios.create({
-      baseURL: "https://vegmiam-back-dev.herokuapp.com/",
-      headers: getHeadersKeys(),
-    });
-  } else if (process.env.NODE_ENV === "production") {
-    return axios.create({
-      baseURL: "https://vegmiam-back-master.herokuapp.com/",
-      headers: getHeadersKeys(),
-    });
-  }
+  const axiosInstance = axios.create({
+    baseURL: "https://vegmiam-back-master.herokuapp.com/",
+    headers: getHeadersKeys(),
+  });
+
+  return axiosInstance;
 };
+
+// export const client = () => {
+//   console.log(process.env.NODE_ENV);
+//   if (process.env.NODE_ENV === "development") {
+//     console.log("dev");
+//     return axios.create({
+//       baseURL: "http://localhost:3000/api/v1/",
+//       headers: getHeadersKeys(),
+//     });
+//   } else if (process.env.NODE_ENV === "devonline") {
+//     console.log("devonline");
+//     return axios.create({
+//       baseURL: "https://vegmiam-back-dev.herokuapp.com/",
+//       headers: getHeadersKeys(),
+//     });
+//   } else if (process.env.NODE_ENV === "production") {
+//     console.log("prod");
+//     return axios.create({
+//       baseURL: "https://vegmiam-back-master.herokuapp.com/",
+//       headers: getHeadersKeys(),
+//     });
+//   }
+// };
 
 ReactDOM.render(
   <React.StrictMode>
